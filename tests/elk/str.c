7e9a8eec772358bef9c5a102d6d36ee9bf141aeb
Ryan <rnleach@users.noreply.github.com>
2025-10-08 13:38:16 -0600

    Add line count function for strings.

--- tests/elk/str.c
+++ tests/elk/str.c
@@ -8,7 +8,7 @@
 static void
 test_from_cstring(void)
 {
-    // Test a regular string
+    /* Test a regular string */
     char *sample = "a sample string";
     ElkStr str = elk_str_from_cstring(sample);
 
@@ -17,7 +17,7 @@ test_from_cstring(void)
     Assert(*(str.start + str.len - 1) == 'g');
     Assert(*(str.start + str.len) == '\0');
 
-    // Test an empty string
+    /* Test an empty string */
     char *empty = "";
     ElkStr empty_str = elk_str_from_cstring(empty);
 
@@ -29,7 +29,7 @@ test_from_cstring(void)
 static void
 test_str_strip(void)
 {
-    // Test with a basic string.
+    /* Test with a basic string. */
     char *sample = "a sample string with puncuation. \" ' ? < > $ % & * ) ( - + = 0123456";
     ElkStr sample_str = elk_str_from_cstring(sample);
 
@@ -39,7 +39,7 @@ test_str_strip(void)
     ElkStr extra_stripped = elk_str_strip(extra_str);
     Assert(elk_str_cmp(sample_str, extra_stripped) == 0);
 
-    // Test with an empty string
+    /* Test with an empty string */
     char *empty = "";
     ElkStr empty_str = elk_str_from_cstring(empty);
     ElkStr empty_strip = elk_str_strip(empty_str);
@@ -49,7 +49,7 @@ test_str_strip(void)
 static void
 test_str_eq(void)
 {
-    // Capitalization comparisons
+    /* Capitalization comparisons */
     char *sample = "a sample string";
     ElkStr sample_str = elk_str_from_cstring(sample);
     char *cap_sample = "A sample string";
@@ -62,13 +62,13 @@ test_str_eq(void)
     Assert(!elk_str_eq(capz_sample_str, sample_str));
     Assert(!elk_str_eq(sample_str, capz_sample_str));
 
-    // Comparison of shorter & longer strings.
+    /* Comparison of shorter & longer strings. */
     char *short_sample = "a sample";
     ElkStr short_sample_str = elk_str_from_cstring(short_sample);
     Assert(!elk_str_eq(short_sample_str, sample_str));
     Assert(!elk_str_eq(sample_str, short_sample_str));
 
-    // Empty string comparisons
+    /* Empty string comparisons */
     char *empty = "";
     ElkStr empty_str = elk_str_from_cstring(empty);
 
@@ -80,7 +80,7 @@ test_str_eq(void)
 static void
 test_str_cmp(void)
 {
-    // Capitalization comparisons
+    /* Capitalization comparisons */
     char *sample = "a sample string";
     ElkStr sample_str = elk_str_from_cstring(sample);
     char *cap_sample = "A sample string";
@@ -93,13 +93,13 @@ test_str_cmp(void)
     Assert(elk_str_cmp(capz_sample_str, sample_str) == -1); // Z comes before a (case sensitive!)
     Assert(elk_str_cmp(sample_str, capz_sample_str) == 1);
 
-    // Comparison of shorter & longer strings.
+    /* Comparison of shorter & longer strings. */
     char *short_sample = "a sample";
     ElkStr short_sample_str = elk_str_from_cstring(short_sample);
     Assert(elk_str_cmp(short_sample_str, sample_str) == -1);
     Assert(elk_str_cmp(sample_str, short_sample_str) == 1);
 
-    // Empty string comparisons
+    /* Empty string comparisons */
     char *empty = "";
     ElkStr empty_str = elk_str_from_cstring(empty);
 
@@ -107,7 +107,7 @@ test_str_cmp(void)
     Assert(elk_str_cmp(sample_str, empty_str) == 1);
     Assert(elk_str_cmp(empty_str, empty_str) == 0);
 
-    // Comparison of copies
+    /* Comparison of copies */
     char sample_copy[16] = {0};
     ElkStr sample_copy_str = elk_str_copy(sizeof(sample_copy), sample_copy, sample_str);
     Assert(elk_str_cmp(sample_copy_str, sample_str) == 0);
@@ -126,27 +126,27 @@ test_str_copy(void)
     ElkStr dest_str = elk_str_copy(sizeof(dest), dest, str);
     ElkStr too_short_str = elk_str_copy(sizeof(too_short), too_short, str);
 
-    Assert(dest_str.len == str.len);           // They're the same size
-    Assert(dest_str.start != str.start);       // They're not the same location in memory
-    Assert(elk_str_eq(dest_str, str));         // They should compare as equals
+    Assert(dest_str.len == str.len);           /* They're the same size                   */
+    Assert(dest_str.start != str.start);       /* They're not the same location in memory */
+    Assert(elk_str_eq(dest_str, str));         /* They should compare as equals           */
     Assert(dest[14] == 'g');
     Assert(dest[15] == '\0');
 
-    Assert(too_short_str.len < str.len);       // Too short is smaller!
-    Assert(too_short_str.start != str.start);  // They're not the same location in memory
-    Assert(!elk_str_eq(too_short_str, str));   // They NOT should compare as equals
+    Assert(too_short_str.len < str.len);       /* Too short is smaller! */
+    Assert(too_short_str.start != str.start);  /* They're not the same location in memory  */
+    Assert(!elk_str_eq(too_short_str, str));   /* They NOT should compare as equals        */
     Assert(too_short[8] == ' ');
-    Assert(too_short[9] == 's');               // NOT a '\0' because we had no extra room.
+    Assert(too_short[9] == 's');               /* NOT a '\0' because we had no extra room. */
 
-    // Test an empty string
+    /* Test an empty string */
     char *empty = "";
     ElkStr empty_str = elk_str_from_cstring(empty);
 
     dest_str = elk_str_copy(sizeof(dest), dest, empty_str);
 
-    Assert(dest_str.len == empty_str.len);     // They're the same size
-    Assert(dest_str.start != empty_str.start); // They're not the same location in memory
-    Assert(elk_str_eq(dest_str, empty_str));   // They should compare as equals
+    Assert(dest_str.len == empty_str.len);     /* They're the same size                   */
+    Assert(dest_str.start != empty_str.start); /* They're not the same location in memory */
+    Assert(elk_str_eq(dest_str, empty_str));   /* They should compare as equals           */
     Assert(dest_str.len == 0);
     Assert(dest[0] == '\0');
 }
@@ -169,6 +169,27 @@ test_str_substr(void)
     Assert(elk_str_eq(sample_str, extra_substr));
 }
 
+static void
+test_str_line_count(void)
+{
+    ElkStr null_str = {0};
+    Assert(elk_str_line_count(null_str) == 0);
+
+    char *one_liner = "Hello, world.";
+    ElkStr one_liner_str = elk_str_from_cstring(one_liner);
+    Assert(elk_str_line_count(one_liner_str) == 1);
+
+    char *paragraph = "This is a\n"
+                      "sample string that is\n"
+                      "multiple lines long.\n"
+                      "Roses are red\n"
+                      "Violets are blue\n"
+                      "This long string sucks\n"
+                      "And so does cancer.";
+    ElkStr paragraph_str = elk_str_from_cstring(paragraph);
+    Assert(elk_str_line_count(paragraph_str) == 7);
+}
+
 /*---------------------------------------------------------------------------------------------------------------------------
  *                                                      All Str tests
  *-------------------------------------------------------------------------------------------------------------------------*/
@@ -181,4 +202,5 @@ elk_str_tests(void)
     test_str_cmp();
     test_str_copy();
     test_str_substr();
+    test_str_line_count();
 }
